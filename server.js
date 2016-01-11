3765ca075e6d14c78d603f6d42b5ae73c2625a93
alessandrofa <alessandro.f.araujo@gmail.com>
2016-01-11 12:02:26 -0200

    Add POST for Todos

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-},
-{
-	id: 2,
-	description: 'Go to Market',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Take children at the school.',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
@@ -38,6 +28,14 @@ app.get('/todos/:id', function (req, res) {
 		res.json(matchedTodo);
 	else
 		res.status(404).send();	
+});
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	
+	res.json(body);
 });
 
 app.listen(PORT, function (){
